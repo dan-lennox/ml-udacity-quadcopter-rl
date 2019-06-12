cd94ba762aa73709c29c6a087898eaabb83edebf
Daniel Lennox <lennox.dan@gmail.com>
2019-06-12 20:38:37 +1000

    refactor: Added comments and clean up.
    - Added additional code comments throughout the agent task.
    - Removed previous reward function attempts.

--- task.py
+++ task.py
@@ -22,227 +22,69 @@ class Task():
 
         self.state_size = self.action_repeat * 6
 
-        # What are these??
         # Min value of each action dimension
         self.action_low = 0
         # Max value of each action dimension
         self.action_high = 900
-        #self.action_high = 1100
 
         #This is the 4 motors... don't change.
         self.action_size = 4 
         
-        self.phase = 0
-        
+        # Set debug flag.
         self.debug = debug
 
-        # Goal
+        # Goal (Fly to a target position).
         self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.]) 
-        
-        self.target_velocity = np.array([1., 1., 1.,]);
 
     def get_reward(self):
         """Uses current pose of sim to return reward."""
-        
-        # TIny reward for just moving towards the target
-        # This is kind of like "taking chess pieces"... free rewards... not great?
-        #reward = 1.-.9*(abs(self.sim.pose[:3] - self.target_pos)).sum()
-        
        
-        
-        #print(reward);
-        
-        #reward = 0
-        #wrong_direction_discount = 1
-        
-        # print(self.sim.pose)
-        #z_distance = abs(self.target_pos[2] - self.sim.pose[2])
-        #distance = abs(self.sim.pose[:3] - self.target_pos)
-
-        
-        #if ((self.sim.v).sum() < 3):
-        #    reward = reward * 0.5
-    
-        #sys.stdout.flush()
-        #print('\r z position', self.sim.pose[2])#, 'z distance', z_distance, 'z target', self.target_pos[2])
-        
-        
-        #if (self.target_pos[2] <= 0):
-        #    wrong_direction_discount = 0.2
-        
-        # Discount for over target.
-        #high_flyer_discount = 1
-        #if (self.sim.pose[2] > (self.target_pos[2] + 2)):
-        #    high_flyer_discount = 0.9
-        #print(self.sim.pose[2])
-        #if (self.sim.pose[2] > 0 and self.sim.pose[2] < self.target_pos[2] + 10):
-        #Small reward for going up in general
-        
-            #reward = 0.1
-            
-            # DO NOT CHANGE THE REWARD FUNCTION!!! TRY TO REPLICATE THE PERFECT RESULT WITH THIS AGAIN
-            # Happened once yesterday.. tweak the actor and the critic.
-            
-        # THE REWARD FUNCTION FOR ALL THE TESTS!!! 27/5/19
-        #if (self.sim.pose[2] > 0):
-            #reward = self.sim.pose[2] * 0.01
-            
-            
-        #if (self.sim.pose[2] > 0):
-        #    reward = self.sim.pose[2] * 0.01
-            
-        #if (self.sim.pose[2] > 1):
-            #reward = 1 - 0.2*(1 / self.sim.pose[2])
-            # Possible to make this a function that equals target? This currently makes
-            # the copter hover at around 42... (where reward is very close to 1 per episode).
-            # NOT VERY SPARSE...
-            
-        #if (self.sim.pose[2] > 11):
-            #reward = 0
-
-        # Large reward for being within range of the target z axis point, that increases the closer we get.
-        #if (z_distance < 5):
-            #reward = 100 / z_distance
-        
-        #print('distance');
-        #print(distance.sum());
-        # Todo: Double check distances are definitely absolute, getting high z values in training.
-        # reward is also going up with higher z values for some reason.. should penalise?
-        # Get largest target value for(x,y,z): get largest
-        #      if largest > target. Penalise strictly.
-        #reward = [distance[0] < 1, distance[1] < 1, distance[2] < 1].count(True)
-        
-        #if (self.sim.v < 0):
-        #   reward 
-        #if (reward == 1):
-            #reward = 0.001;
-        
-        #if (reward == 2):
-            #reward = 0.01;
-            
-        #if (reward == 3):
-        #    reward = 1;
-        #else:
-        #    reward = 0
-        
-         # Euclidean distance. Source?
-        #distance = abs(np.linalg.norm(self.sim.pose[:3]-self.target_pos))
-        #reward = 1/distance
-        #reward = -abs(np.linalg.norm(self.sim.pose[:3]-self.target_pos))
-        #reward = -np.linalg.norm(self.sim.pose[:3]-self.target_pos)
-        #reward = -np.linalg.norm(self.sim.pose[:3]-self.target_pos)
-        #reward = np.clip(2.-.25*(np.linalg.norm(self.sim.pose[:3]-self.target_pos)), -1, 1)
-        
+        # Record the distance vector between the current copter position and the target position.
         distance = abs(self.sim.pose[:3] - self.target_pos)
+         
+        # Record the euclidean distance between the current copter position and the target position.
+        ec_dist = abs(np.linalg.norm(self.sim.pose[:3]-self.target_pos))
+
+        # Apply a small reward for progress towards the target.
+        # Reward scales to penalise further distances heavily.
+        reward = (1-(np.power(ec_dist, 2) / 100) + 2)
         
-        #print('max', max(distance))
-        
-        # Emphasis penalty on further distances.
-        # How to emphasis reward on better results?
-        ec_dist = abs(np.linalg.norm(self.sim.pose[:3]-self.target_pos)) # THE GOOD RESULT
-        #reward = (1-(np.power(max(distance), 2) / 100) + 2) * 2
-        reward = (1-(np.power(ec_dist, 2) / 100) + 2) # THE GOOD RESULT
-        #reward = 1-ec_dist
-        #reward = 1-ec_dist
-        
-        
-        
-#        if (ec_dist < 5):
- #           reward
-        
-        #print(distance);
-        #print(reward)
-        #print('relu', (abs(reward) + reward))
-        
-        #print(np.linalg.norm(self.sim.pose[:3]-self.target_pos));
-        #print(-np.tanh(reward))
-        #print(distance.sum());
-        #print(np.tanh(np.sinh(reward)));
-        #print('----------')
-        
-        # End the episode if we reach the target.
-        # Maybe remove this AFTER I can reach targets, then I can work on hovering.
+        # Record if the coptor came within one unit of the target in all dimensions. This is
+        # counted as "reaching the target".
         reached_target = ([distance[0] < 1, distance[1] < 1, distance[2] < 1].count(True) == 3);
+
         if (reached_target):
-        #if (reward > -2):
             if (self.debug):
                 print('REACHED TARGET THIS EPISODE')
-            #print('distance', distance)
-            #print('reward', reward)
-            # THIS IS WORKING CORRECTY. VERIFIED IN TRAINER.
+
+            # End this episode early if the target was reached.
             self.sim.done = True
-            # Makes sense.. no more "future rewards" for the agent to consider and abuse.
+            
+            # Large reward for actually reaching the target.
             return 100
-        # Check if done is true before the runtime finished
-        # crash penalty! https://github.com/WittmannF/quadcopter-best-practices
+        
+        # Check to see if the copter crashed.
         elif (self.sim.done and self.sim.runtime > self.sim.time):
             if (self.debug):
                 print('\nCRASHED')
-            #print('distance', distance)
-            #print('reward', reward)
-            # THIS IS WORKING CORRECTY. VERIFIED IN TRAINER.
+            # Penalise heavily for crashes.
+            # (Removed as performance worked better without this negative reward, see below for
+            # the positive NOT crashing reward which replaced this behaviour.
             #return -100
         
-        # Give reward for not crashing?
+        # If the copter is still in the air this timestep and hasn't crashed!
         if ((self.sim.runtime - self.sim.time) < 0):
+            # Double the reward given based on the distance to the target.
             if (reward < 0):
                 reward = reward / 2
             if (reward > 0):
                 reward = reward * 2
-            #reward += 2
+
             if (self.debug):
                 print('\nNO CRASH')
-            #print(self.sim.runtime - self.sim.time)
-            #reward = reward + 10
-        
-       
-        
-        #print('vel', np.mean(self.sim.v))
-        #if (np.mean(self.sim.v > 0) and np.mean(self.sim.v < 1)):
-        #   reward = reward + 2
         
         # ReLu activation for positive rewards.
-        return (abs(reward) + reward) / 2 #np.tanh(reward)
-        
-            #velocity discount?
-     
-        
-        #print(reward)
-        #reward = 0.001*(reward**9)
-        
-        #velocity_discount = 1.-.9*(abs(self.sim.v - self.target_velocity)).sum()
-        #print(velocity_discount)
-        #print(self.sim.v)
-        #reward = reward / velocity_discount
-        #if (self.sim.v < 0):
-        #    reward = 0
-        
-        #if (self.sim.v > 1):
-        #    reward = 0
-    
-            
-        #nice idea, but too much reward for nothing!! 2... Can I make it exponential?
-                       
-        #if (distance[0] < 1 and distance[1] < 1 and distance[2] < 1):
-        #    reward = 1
-        
-        #if (distance[2] < 1):
-            
-            
-        # let's try the x and y penalties.
-        #if (self.sim.pose[0] < 1 or self.sim.pose[0] > -1):
-            #reward = 1
-            
-        #if (self.sim.pose[1] < 1 or self.sim.pose[1] > -1):
-            #reward = 1
-       
-        #WHY is the average reward still increasing past 15 height??
-        # Ah... because rewards after about 10 get very close to 1!! Which are good rewards...
-        # Maybe just give a flat reward?
-        
-        # tanh scales reward to -1 to 1.
-        #return reward
-   
+        return (abs(reward) + reward) / 2
 
     def step(self, rotor_speeds):
         """Uses action to obtain next state, reward, done."""
